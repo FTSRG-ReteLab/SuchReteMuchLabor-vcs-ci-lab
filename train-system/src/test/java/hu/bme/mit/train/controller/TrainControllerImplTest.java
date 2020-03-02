a033045720583b129409ff2b8a311bfe2b70cd78
G6GE25 <bencebarat77@gmail.com>
2020-03-02 17:16:22 +0100

    Travis update

--- train-system/src/test/java/hu/bme/mit/train/controller/TrainControllerImplTest.java
+++ train-system/src/test/java/hu/bme/mit/train/controller/TrainControllerImplTest.java
@@ -1,5 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package hu.bme.mit.train.controller;
+
+import org.junit.Test;
 
 class TrainControllerImplTest {
+    @Test
+    public void WrongHotfix()
+    {
+        TrainControllerImpl tmp=new TrainControllerImpl();
+        if(tmp.neededhotfix!=1) System.out.println("bad hotfix");
+        else System.out.println("good hotfix");
+    }
 
 }
